8de52388b0b063f3e721a80b38d8486cbd8b7d98
Vivien Clauzon <xr_a_y@yahoo.fr>
2018-10-29 16:36:36 +0100

    stripped version

--- tools/build.sh
+++ tools/build.sh
@@ -5,4 +5,4 @@ if [ -n "$1" ] ; then
 else
    v="dev"
 fi
-g++ -DNDEBUG -O3 -flto -msse4.2 --std=c++11 minic.cc -o $dir/Dist/minic_${v}_linux_x64_see4.2
+g++ -s -DNDEBUG -O3 -flto -msse4.2 --std=c++11 minic.cc -o $dir/Dist/minic_${v}_linux_x64_see4.2
--- tools/buildGW.sh
+++ tools/buildGW.sh
@@ -6,3 +6,4 @@ else
    v="dev"
 fi
 x86_64-w64-mingw32-g++ minic.cc -O3 -flto -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/minic_${v}_mingw_x64.exe
+x86_64-w64-mingw32-strip $dir/Dist/minic_${v}_mingw_x64.exe
