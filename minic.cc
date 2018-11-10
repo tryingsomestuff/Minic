5e0a840ef25ea12f87e72ea327093db0de6efaab
Vivien Clauzon <xr_a_y@yahoo.fr>
2018-11-10 12:09:06 +0100

    version

--- minic.cc
+++ minic.cc
@@ -23,7 +23,7 @@ typedef uint64_t u_int64_t;
 
 //#define IMPORTBOOK
 
-const std::string MinicVersion = "0.14";
+const std::string MinicVersion = "0.15";
 
 typedef std::chrono::high_resolution_clock Clock;
 typedef char DepthType;
