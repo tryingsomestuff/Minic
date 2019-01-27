fb4ef95ef174a55dfef81dbc8779eb4e7d55d35e
Vivien Clauzon <xr_a_y@yahoo.fr>
2019-01-27 09:16:40 +0100

    release 0.34

--- minic.cc
+++ minic.cc
@@ -32,7 +32,7 @@ typedef uint64_t u_int64_t;
 //#define WITH_TEST_SUITE
 //#define WITH_SYZYGY
 
-const std::string MinicVersion = "dev";
+const std::string MinicVersion = "0.34";
 
 #define STOPSCORE   ScoreType(20000)
 #define INFSCORE    ScoreType(15000)
