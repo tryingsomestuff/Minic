8ff199c464d15749ed86169b6e5b0c8ed5c25bc2
Vivien Clauzon <vivien.clauzon@gmail.com>
2019-04-26 13:14:00 +0200

    fix build again

--- tools/buildGW32.sh
+++ tools/buildGW32.sh
@@ -20,14 +20,13 @@ echo "version $v"
 echo "definition $d"
 echo "target $t"
 
-exe=minic_${v}_mingw_x64
+exe32=minic_${v}_mingw_x32
 if [ "$t" != "-march=native" ]; then
    tname=$(echo $t | sed 's/-m//g' | sed 's/arch=//g' | sed 's/ /_/g')
-   exe=${exe}_${tname}
+   exe32=${exe32}_${tname}
 fi
-exe=${exe}.exe
-echo "Building $exe"
+exe32=${exe32}.exe
+echo "Building $exe32"
 
-x86_64-w64-mingw32-g++ minic.cc $d -DNDEBUG -O3 -flto $t -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/$exe -Wl,-Bstatic -lpthread
-x86_64-w64-mingw32-strip $dir/Dist/$exe
-
+i686-w64-mingw32-g++-posix minic.cc $d -DNDEBUG -O3 -flto $t -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/$exe32 -lpthread
+i686-w64-mingw32-strip $dir/Dist/$exe32
--- tools/release.sh
+++ tools/release.sh
@@ -7,6 +7,10 @@ $dir/build.sh $v "-march=x86-64"
 $dir/build.sh $v "-msse4.2"
 $dir/build.sh $v "-mavx2 -mbmi2"
 
-$dir/buildGW.sh $v "-march=i686"
+$dir/buildGW.sh $v "-march=x86-64"
 $dir/buildGW.sh $v "-msse4.2"
 $dir/buildGW.sh $v "-mavx2 -mbmi2"
+
+$dir/buildGW32.sh $v "-march=i686"
+$dir/buildGW32.sh $v "-msse4.2"
+$dir/buildGW32.sh $v "-mavx2 -mbmi2"
