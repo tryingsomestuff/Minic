bcfb5ee546186497aa6365574c6a0942cd10ea3e
Vivien Clauzon <vivien.clauzon@gmail.com>
2019-04-26 11:47:02 +0200

    fix build

--- tools/buildGW.sh
+++ tools/buildGW.sh
@@ -28,7 +28,7 @@ fi
 exe=${exe}.exe
 echo "Building $exe"
 
-x86_64-w64-mingw32-g++ minic.cc $d -DNDEBUG -O3 -flto -t -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/$exe -Wl,-Bstatic -lpthread
+x86_64-w64-mingw32-g++ minic.cc $d -DNDEBUG -O3 -flto $t -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/$exe -Wl,-Bstatic -lpthread
 x86_64-w64-mingw32-strip $dir/Dist/$exe
 
 exe32=minic_${v}_mingw_x32
@@ -39,5 +39,5 @@ fi
 exe32=${exe32}.exe
 echo "Building $exe32"
 
-i686-w64-mingw32-g++-posix minic.cc $d -DNDEBUG -O3 -flto -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/$exe32 -lpthread
+i686-w64-mingw32-g++-posix minic.cc $d -DNDEBUG -O3 -flto $t -static -static-libgcc -static-libstdc++ -std=c++11 -o $dir/Dist/$exe32 -lpthread
 i686-w64-mingw32-strip $dir/Dist/$exe32
