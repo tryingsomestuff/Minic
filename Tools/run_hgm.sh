c7dd2a35e749ea0235f84e1634bc3768d2da83b1
Vivien <xr_a_y@yahoo.fr>
2020-05-13 14:09:12 +0200

    fix ics script

--- Tools/run_hgm.sh
+++ Tools/run_hgm.sh
@@ -2,5 +2,5 @@ dir=$(readlink -f $(dirname $0)/../)
 echo $dir/Tourney/minic_dev_linux_x64
 ls $dir/Tourney/minic_dev_linux_x64
 #xboard -zp -ics -icshost winboard.nl -icshelper timeseal -fcp "./minic_dev_linux_x64 -xboard" -fd "$dir/Tourney/" -autoKibitz -debug 
-xboard -zp -ics -icshost winboard.nl -fcp "./minic_dev_linux_x64 -xboard" -fd "$dir/Tourney/" -autoKibitz -debug 
-
+#xboard -zp -ics -icshost winboard.nl -fcp "./minic_dev_linux_x64 -xboard" -fd "$dir/Tourney/" -autoKibitz -debug 
+xboard -zp -ics -icshost nightmare-chess.nl -fcp "./minic_dev_linux_x64 -xboard" -fd "$dir/Tourney/" -autoKibitz -debug
