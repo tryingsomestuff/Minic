2b84dbca1248808734937b76341b997a3683ea65
Vivien <xr_a_y@yahoo.fr>
2020-05-13 14:09:38 +0200

    use a default contemp

--- Source/dynamicConfig.cpp
+++ Source/dynamicConfig.cpp
@@ -16,8 +16,8 @@ namespace DynamicConfig{
     bool FRC               = false;
     bool UCIPonder         = false;
     unsigned int multiPV   = 1;
-    ScoreType contempt     = 0;
-    ScoreType contemptMG   = 0;
+    ScoreType contempt     = 15;
+    ScoreType contemptMG   = 15;
     bool limitStrength     = false;
     int strength           = 1500;
 }
