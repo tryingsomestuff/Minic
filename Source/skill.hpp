ea8b8cad7d5b62f14994f0eb542124a5881c007f
Vivien <xr_a_y@yahoo.fr>
2020-05-13 15:00:24 +0200

    fix compil warning

--- Source/skill.hpp
+++ Source/skill.hpp
@@ -7,7 +7,7 @@
 // from Stockfish implementation
 namespace Skill {
     inline bool enabled() { return DynamicConfig::level > 0 && DynamicConfig::level < 100; } // 0 is random mover !
-    inline DepthType limitedDepth() { return DepthType(1 + 2 * std::sqrt(std::max(0u,DynamicConfig::level-20))); }
+    inline DepthType limitedDepth() { return DepthType(1 + 2 * std::sqrt(std::max(0,int(DynamicConfig::level-20)))); }
     Move pick(std::vector<RootScores> & multiPVMoves);
     static inline unsigned int Elo2Level(){ return std::max(0,(DynamicConfig::strength - 500))/29;}
 }
