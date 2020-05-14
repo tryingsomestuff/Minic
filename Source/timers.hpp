ec249e9c3f7a1fffb342e654493bcee39e2bd391
Vivien <xr_a_y@yahoo.fr>
2020-05-14 18:24:59 +0200

    bcleanertimers

--- Source/timers.hpp
+++ Source/timers.hpp
@@ -8,31 +8,13 @@
 #ifdef WITH_TIMER
 uint64_t rdtsc();
 #define START_TIMER uint64_t rdtscBegin = rdtsc();
-#define STOP_AND_SUM_TIMER(name) Timers::rdtscCounter##name += rdtsc() - rdtscBegin; ++Timers::callCounter##name;
+#define STOP_AND_SUM_TIMER(name) Timers::rdtscCounter[TM_##name] += rdtsc() - rdtscBegin; ++Timers::callCounter[TM_##name];
+
+enum TimerType : unsigned char { TM_Total=0, TM_See, TM_Apply, TM_Eval1, TM_Eval2, TM_Eval3, TM_Eval4, TM_Eval5, TM_Eval, TM_Attack, TM_MovePiece, TM_Generate, TM_PseudoLegal, TM_IsAttacked, TM_MoveScoring, TM_MoveSorting, TM_Max};
 
 namespace Timers{
-extern uint64_t rdtscCounterSee        ;
-extern uint64_t rdtscCounterApply      ;
-extern uint64_t rdtscCounterEval       ;
-extern uint64_t rdtscCounterAttack     ;
-extern uint64_t rdtscCounterMovePiece  ;
-extern uint64_t rdtscCounterGenerate   ;
-extern uint64_t rdtscCounterPseudoLegal;
-extern uint64_t rdtscCounterIsAttacked ;
-extern uint64_t rdtscCounterMoveSorting;
-extern uint64_t rdtscCounterTotal      ;
- 
-extern uint64_t callCounterSee        ;
-extern uint64_t callCounterApply      ;
-extern uint64_t callCounterEval       ;
-extern uint64_t callCounterAttack     ;
-extern uint64_t callCounterMovePiece  ;
-extern uint64_t callCounterGenerate   ;
-extern uint64_t callCounterPseudoLegal;
-extern uint64_t callCounterIsAttacked ;
-extern uint64_t callCounterMoveSorting;
-extern uint64_t callCounterTotal      ;
-
+extern uint64_t rdtscCounter[TM_Max];
+extern uint64_t callCounter[TM_Max];
 void Display();
 }
 #else
