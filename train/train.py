8eb3be33ad59db89eb9f9e09dcda55c05450d6b9
Vivien <xr_a_y@yahoo.fr>
2020-11-28 12:50:50 +0100

    profilter for NN training

--- train/train.py
+++ train/train.py
@@ -6,8 +6,8 @@ from torch import set_num_threads as t_set_num_threads
 from pytorch_lightning import loggers as pl_loggers
 from torch.utils.data import DataLoader, Dataset
 
-def data_loader_py(train_filename, val_filename, batch_size):
-  train = DataLoader(nnue_bin_dataset.NNUEBinData(train_filename), batch_size=batch_size, shuffle=True, num_workers=4)
+def data_loader_py(train_filename, val_filename, num_workers, batch_size):
+  train = DataLoader(nnue_bin_dataset.NNUEBinData(train_filename), batch_size=batch_size, shuffle=True, num_workers=num_workers)
   val = DataLoader(nnue_bin_dataset.NNUEBinData(val_filename), batch_size=32)
   return train, val
 
@@ -16,7 +16,6 @@ def main():
   parser.add_argument("train", help="Training data (.bin or .binpack)")
   parser.add_argument("val", help="Validation data (.bin or .binpack)")
   parser = pl.Trainer.add_argparse_args(parser)
-  parser.add_argument("--py-data", action="store_true", help="Use python data loader (default=False)")
   parser.add_argument("--lambda", default=1.0, type=float, dest='lambda_', help="lambda=1.0 = train on evaluations, lambda=0.0 = train on game results, interpolates between (default=1.0).")
   parser.add_argument("--num-workers", default=1, type=int, dest='num_workers', help="Number of worker threads to use for data loading. Currently only works well for binpack.")
   parser.add_argument("--batch-size", default=-1, type=int, dest='batch_size', help="Number of positions per batch / per iteration. Default on GPU = 8192 on CPU = 128.")
@@ -36,16 +35,12 @@ def main():
     print('limiting torch to {} threads.'.format(args.threads))
     t_set_num_threads(args.threads)
 
-  if args.py_data:
-    print('Using python data loader')
-    train, val = data_loader_py(args.train, args.val, batch_size)
-  else:
-    print('Using c++ data loader')
-    train, val = data_loader_cc(args.train, args.val, args.num_workers, batch_size)
+  print('Using python data loader')
+  train, val = data_loader_py(args.train, args.val, args.num_workers, batch_size)
 
   tb_logger = pl_loggers.TensorBoardLogger('logs/')
   checkpoint_callback = pl.callbacks.ModelCheckpoint(save_last=True)
-  trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint_callback], logger=tb_logger)
+  trainer = pl.Trainer.from_argparse_args(args, callbacks=[checkpoint_callback], logger=tb_logger, profiler='advanced')
   trainer.fit(nnue, train, val)
 
 if __name__ == '__main__':
