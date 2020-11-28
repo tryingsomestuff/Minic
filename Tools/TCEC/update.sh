abb80336e0dbc65d1ede7d09e59a3e696d815167
Vivien <xr_a_y@yahoo.fr>
2020-11-28 12:57:58 +0100

    add TCEC build script and config

---
+++ Tools/TCEC/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone --branch=$VERSION https://github.com/tryingsomestuff/Minic
+cd Minic
+git clone https://github.com/jdart1/Fathom.git
+read -p "Download done, press enter to continue"
+sed -i.bak 's/OPT="-s /OPT="-g /' ./Tools/build.sh
+./Tools/build.sh $VERSION "-mavx2 -mbmi2"
+cd Dist/Minic3
+ln -s minic_${VERSION}_linux_x64_avx2_bmi2 minic_linux_x64_avx2_bmi2
+EXE=$PWD/minic_linux_x64_avx2_bmi2
+
