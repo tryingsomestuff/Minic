22bef4d047bdb2323bf940cdb55bdb62606bfda1
Vivien <xr_a_y@yahoo.fr>
2021-01-24 19:18:18 +0100

    fix gen script

--- Tools/gen.sh
+++ Tools/gen.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-dir=$(readlink -f $(dirname $0/../))
+dir=$(readlink -f $(dirname $0)/../)
 
 for i in $(seq 1 10000); do 
   $dir/Dist/Minic3/minic_dev_linux_x64 -selfplay 5 10000 -genFenDepth 8 -randomOpen 50 
