239013e134c97e26042a8e002b975d804794f58b
Vivien <xr_a_y@yahoo.fr>
2021-02-07 15:43:40 +0100

    fix plot

--- train/plot.py
+++ train/plot.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(description='NNUE training plots.')
 parser.add_argument('-s', type=int, help='skip this net at first', default=0 )
 parser.add_argument('-k', type=int, help='only keep this last nets', default=0)
 parser.add_argument('-p', type=int, help='pause between refresh in sec', default=15)
+parser.add_argument('-y', type=int, help='y axis low limit', default=-150)
 parser.add_argument('-m', type=int, help='first target', default=20, action=TargetBoundCheck)
 parser.add_argument('-l', type=int, help='second target', default=40, action=TargetBoundCheck)
 parser.add_argument('-f', type=str, help='ordo file path', default='logs/ordo.out')
@@ -55,65 +56,50 @@ while True:
             plt.subplot(121)
 
             plt.errorbar(X, Y, yerr=err, fmt='-.', color='gray', marker=None)
-            plt.scatter(X, Y, c=['gold' if y-e>0 else 'lime' if y>0 else 'black' if y+e > 0 else 'red' for y,e in zip(Y,err)], s=64, marker='o', label='Nets Elo')
+            plt.scatter(X, Y, c=['gold' if y-e>args.l else 'silver' if y-e>args.m else 'lime' if y-e>0 else 'cyan' if y>0 else 'black' if y+e > 0 else 'red' for y,e in zip(Y,err)], s=64, marker='o', label='Nets Elo')
             plt.scatter(X, [ y+1.5*e for (y,e) in zip(Y,err)], color='violet', marker='x', label='Best luck (150%err)')
             plt.axhline(y=0, color='tomato', linestyle='-', label='Current master')
             plt.axhline(y=args.m, color='deepskyblue', linestyle='dotted', label='Master + {}Elo'.format(args.m))
             plt.axhline(y=args.l, color='springgreen', linestyle='dashed', label='Master + {}Elo'.format(args.l))
             plt.axhline(y=max([y-e for (y,e) in zip(Y,err)]), color='aqua', linestyle='dashdot', label='Current worst outcome')
             axes = plt.gca()
-            axes.set_ylim([-250,1.5*max(args.m,args.l)])
+            axes.set_ylim([args.y,1.5*max(args.m,args.l)])
 
             plt.legend()
 
             display_max=30
 
             now = datetime.now()
-            txt = now.strftime("%m/%d/%Y, %H:%M:%S") + '\n\n'
-            target2 = 0
-            target1 = 0
-            good = 0
-            probable = 0
-            possible = 0
+            txt = [now.strftime("%m/%d/%Y, %H:%M:%S\n")]
+            target2 = []
+            target1 = []
+            good = []
+            probable = []
+            possible = []
             for (x, y, e, n) in zip(X, Y, err, names): 
                 if y - e > args.l:
-                    txt += 'target2 net {: <5} +/- {: <5} : {}'.format(y,e,n) + '\n'
-                    target2+=1
-                    if target2 > display_max:
-                        break         
-            if target2 < display_max:
-                for (x, y, e, n) in zip(X, Y, err, names): 
-                    if y - e > args.m:
-                        txt += 'target1 net {: <5} +/- {: <5} : {}'.format(y,e,n) + '\n'
-                        target1+=1            
-                    if target1+target2 > display_max:
-                        break         
-            if target1+target2 < display_max:
-                for (x, y, e, n) in zip(X, Y, err, names): 
-                    if y - e > 0:
-                        txt += 'good net {: <5} +/- {: <5} : {}'.format(y,e,n) + '\n'
-                        good+=1
-                    if good+target1+target2 > display_max:
-                        break         
-            if target1+target2+good < display_max:
-                for (x, y, e, n) in zip(X, Y, err, names): 
-                    if y > 0:
-                        txt +=  'probable net {: <5} +/- {: <5} : {}'.format(str(y),str(e),n) + '\n'
-                        probable+=1
-                    if probable+good+target1+target2 > display_max:
-                        break         
-            if target1+target2+good+probable < display_max:
-                for (x, y, e, n) in zip(X, Y, err, names): 
-                    if y + e > 0:
-                        txt += 'possible net {: <5} +/- {: <5} : {}'.format(str(y),str(e),n) + '\n'
-                        possible+=1
-                    if possible+probable+good+target1+target2 > display_max:
-                        break         
+                    target2.append([x,y,e,n])
+                elif y - e > args.m:
+                    target1.append([x,y,e,n])
+                elif y - e > 0:
+                    good.append([x,y,e,n])
+                elif y > 0:
+                    probable.append([x,y,e,n])
+                elif y + e > 0:
+                    possible.append([x,y,e,n])
+
+            txt += ['target2  net {: <5} +/- {: <5} : {}'.format(y,e,n) for (x,y,e,n) in target2 ]
+            txt += ['target1  net {: <5} +/- {: <5} : {}'.format(y,e,n) for (x,y,e,n) in target1 ]
+            txt += ['good     net {: <5} +/- {: <5} : {}'.format(y,e,n) for (x,y,e,n) in good ]
+            txt += ['probable net {: <5} +/- {: <5} : {}'.format(y,e,n) for (x,y,e,n) in probable ]
+            txt += ['possible net {: <5} +/- {: <5} : {}'.format(y,e,n) for (x,y,e,n) in possible ]
+
+            txt = txt[:display_max+2]
 
             plt.subplot(122)
             plt.axis('off')
             size = plt.gcf().get_size_inches()*plt.gcf().dpi # size in pixels
-            plt.text(0, 0, txt, fontsize=size[1]/max(2*display_max+10,2*txt.count('\n')+10))
+            plt.text(0, 0, '\n'.join(txt), fontsize=size[1]/max(2*display_max+10,2*txt.count('\n')+10))
 
             plt.show(block=False)
             plt.pause(args.p)
