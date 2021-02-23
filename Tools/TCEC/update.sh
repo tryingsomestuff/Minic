1cb257751d59acc4f0e280cb71828cec1a6454a1
Vivien <xr_a_y@yahoo.fr>
2021-02-23 17:49:44 +0100

    update TCEC scripts

--- Tools/TCEC/update.sh
+++ Tools/TCEC/update.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+
+NETNAME=noisy_notch.bin
+
+# get minic
 git clone --branch=$VERSION https://github.com/tryingsomestuff/Minic
 cd Minic
-wget https://raw.githubusercontent.com/tryingsomestuff/NNUE-Nets/master/noisy_notch.bin
+
+# get net
+wget https://raw.githubusercontent.com/tryingsomestuff/NNUE-Nets/master/$NETNAME
 mkdir -p Tourney
-cp noisy_notch.bin Tourney/nn.bin
+cp $NETNAME Tourney/nn.bin
+
+# get fathom
 git clone https://github.com/jdart1/Fathom.git
 read -p "Download done, press enter to continue"
+
+# build
 sed -i.bak 's/OPT="-s /OPT="-g /' ./Tools/build.sh
 ./Tools/build.sh $VERSION "-mavx2 -mbmi2"
 cd Dist/Minic3
 ln -s minic_${VERSION}_linux_x64_avx2_bmi2 minic_linux_x64_avx2_bmi2
-EXE=$PWD/minic_linux_x64_avx2_bmi2
 
+EXE=$PWD/minic_linux_x64_avx2_bmi2
+cd ../..
+NET=$PWD/$NETNAME
+
+# bench
+$EXE bench 20 -NNUEFile $NET
