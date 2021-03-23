e2798633144cac4a88d756077ccb5e60c9b9ca53
Vivien <xr_a_y@yahoo.fr>
2021-03-23 14:28:28 +0100

    fix visualization

--- train/visualize.py
+++ train/visualize.py
@@ -5,33 +5,34 @@ import matplotlib.pyplot as plt
 
 import model
 
-
 def to_image(x):
   print(x.size())
   # encoding ---- king square ---- piece type ---- piece square
-  x = x.reshape(8,8,  8,8,  2,6,  8,8)
+  x = x.reshape(8,16,  8,8,  2,6,  8,8)
 
   # height ---- width
   x = x.permute(0,5,6,2,  1,4,7,3)
-  x = (x.reshape(4*6*8*8, 16*2*8*8)*10).abs().sigmoid()
+  x = (x.reshape(8*6*8*8, 16*2*8*8) * 3.0).abs().sigmoid()
   return x
 
 
 def plot_input_weights_image(x, name, dst):
   title = f'abs(feature transformer) [{name}]'
+  cmap = 'hot'
+
+  #dst.
   dst.set_title(title)
-  dst.matshow(x.detach().numpy(), cmap='viridis')
+  dst.matshow(x, cmap=cmap)
   
-  line_options = {'color': 'red', 'linewidth': 1}
+  line_options = {'color': 'white', 'linewidth': 1}
   for i in range(1, 16):
     dst.axvline(x=2*8*8*i-1, **line_options)
 
-  for j in range(1, 4):
+  for j in range(1, 8):
     dst.axhline(y=6*8*8*j-1, **line_options)
 
-
 def main():
-  DPI = 150
+  DPI = 300
 
   if len(sys.argv) < 2:
     print("Usage : python visualize.py net")
@@ -51,8 +52,8 @@ def main():
   nnue.cpu()
   nnue.eval()
 
-  w = to_image(nnue.white_affine.weight)
-  b = to_image(nnue.black_affine.weight)
+  w = to_image(nnue.white_affine.virtual_weight())
+  b = to_image(nnue.black_affine.virtual_weight())
 
   fig, (axs_w, axs_b) = plt.subplots(1, 2, figsize=(2*w.size(1) // DPI, w.size(0) // DPI), dpi=DPI)
   plot_input_weights_image(w, 'white', axs_w)
