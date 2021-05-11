707c66ee82f87e09f4474352f76812359b8771bb
Vivien <xr_a_y@yahoo.fr>
2021-05-11 19:29:17 +0000

    update gen script

--- Tools/NNUE/gen.sh
+++ Tools/NNUE/gen.sh
@@ -2,6 +2,6 @@
 dir=$(readlink -f $(dirname $0)/../../)
 
 for i in $(seq 1 10000); do 
-  $dir/Dist/Minic3/minic_dev_linux_x64 -selfplay 5 10000 -genFenDepth 8 -randomOpen 50 
+  $dir/Dist/Minic3/minic_dev_linux_x64 -selfplay 5 10000 -genFenDepth 8 -genFenDepthEG 12 -randomOpen 250 -FRC
 done
 
