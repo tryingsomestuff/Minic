959a507289180a4a78d8f1223fa9b72ba8250370
Vivien <xr_a_y@yahoo.fr>
2021-08-07 17:57:05 +0200

    update version

--- Source/definition.hpp
+++ Source/definition.hpp
@@ -38,7 +38,7 @@ typedef uint64_t u_int64_t;
 #include <unistd.h>
 #endif
 
-const std::string MinicVersion = "3.10";
+const std::string MinicVersion = "3.11";
 
 // *** options
 #define WITH_UCI    // include or not UCI protocol support
