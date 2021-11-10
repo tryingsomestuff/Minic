5823dea9a00eddbce14f34fa27313816a35da882
Vivien <xr_a_y@yahoo.fr>
2021-11-10 21:40:51 +0100

    update some engine rating

--- Source/opponent.cpp
+++ Source/opponent.cpp
@@ -7,22 +7,22 @@
 namespace Opponent {
 
 static std::map<std::string, uint16_t> ratings = {
-    {"Fat Fritz 2", 3639}, {"Stockfish", 3657}, {"Dragon", 3632},       {"Lc0", 3524},        {"Allie", 3445},         {"Houdini", 3443},
-    {"Nemorino", 3437},    {"RubiChess", 3457}, {"SlowChess", 3474},    {"Igel", 3428},       {"Ethereal", 3458},      {"Stoofvlees", 3350},
-    {"Revenge", 3433},     {"Xiphos", 3341},    {"Fire", 3340},         {"Booot", 3321},      {"RofChade", 3305},      {"Defenchess", 3283},
-    {"Schooner", 3271},    {"Laser", 3273},     {"Shredder", 3270},     {"Fizbo", 3248},      {"Andscacs", 3230},      {"Minic", 3253},
-    {"Wasp", 3219},        {"Roc", 3218},       {"DanaSah", 3255},      {"Halogen", 3300},    {"Arasan", 3300},        {"Combusken", 3169},
+    {"Fat Fritz 2", 3636}, {"Stockfish", 3684}, {"Dragon", 3637},       {"Lc0", 3523},        {"Allie", 3443},         {"Houdini", 3442},
+    {"Nemorino", 3436},    {"RubiChess", 3456}, {"SlowChess", 3488},    {"Igel", 3424},       {"Ethereal", 3513},      {"Stoofvlees", 3349},
+    {"Revenge", 3439},     {"Xiphos", 3340},    {"Fire", 3437},         {"Booot", 3315},      {"RofChade", 3304},      {"Defenchess", 3284},
+    {"Schooner", 3271},    {"Laser", 3273},     {"Shredder", 3270},     {"Fizbo", 3248},      {"Andscacs", 3229},      {"Minic", 3253},
+    {"Wasp", 3219},        {"Roc", 3218},       {"DanaSah", 3254},      {"Halogen", 3300},    {"Arasan", 3374},        {"Combusken", 3169},
     {"Demolito", 3168},    {"Chiron", 3164},    {"NirvanaChess", 3164}, {"Equinox", 3161},    {"Critter", 3156},       {"Winter", 3155},
-    {"Marvin", 3145},      {"Strelka", 3142},   {"Orion", 3138},        {"PanChess", 3138},   {"Toga", 3135},          {"Vajolet2", 3128},
-    {"Weiss", 3314},       {"Koivisto", 3462},  {"Gull", 3117},         {"Texel", 3114},      {"Hannibal", 3110},      {"Rybka", 3110},
-    {"Stash", 3142},       {"iCE", 3108},       {"chess22k", 3097},     {"BlackMamba", 3091}, {"Protector", 3088},     {"Seer", 3373},
+    {"Marvin", 3145},      {"Strelka", 3142},   {"Orion", 3138},        {"PanChess", 3138},   {"Toga", 3201},          {"Vajolet2", 3128},
+    {"Weiss", 3312},       {"Koivisto", 3507},  {"Gull", 3117},         {"Texel", 3114},      {"Hannibal", 3110},      {"Rybka", 3110},
+    {"Stash", 3142},       {"iCE", 3108},       {"chess22k", 3097},     {"BlackMamba", 3091}, {"Protector", 3088},     {"Seer", 3370},
     {"Senpai", 3085},      {"Pirarucu", 3060},  {"ChessBrainVB", 3050}, {"SmarThink", 3045},  {"FabChess", 3033},      {"Monolith", 3032},
     {"Alfil", 3031},       {"Naum", 3031},      {"Bagatur", 3024},      {"Tucano", 3022},     {"Hakkapeliitta", 3021}, {"Deuterium", 3020},
     {"Amoeba", 3019},      {"Rodent", 3005},    {"Cheng", 3000},        {"Gogobello", 2991},  {"Topple", 2987},        {"Hiarcs", 2985},
     {"Counter", 2982},     {"Godel", 2982},     {"PeSTO", 2979},        {"Mr Bob", 2971},     {"Crafty", 2965},        {"Bobcat", 2961},
     {"BBC", 2957},         {"Francesca", 2941}, {"Dirty", 2928},        {"Deep", 2926},       {"Spark", 2926},         {"Asymptote", 2924},
     {"Atlas", 2923},       {"Invictus", 2923},  {"Spike", 2920},        {"EXchess", 2916},    {"DiscoCheck", 2915},    {"tomitankChess", 2906},
-    {"Scorpio", 2904},     {"Berserk", 3322},   {"Quazar", 2901},       {"Zurichess", 2901},  {"Deep", 2898},          {"Drofa", 2894},
+    {"Scorpio", 2904},     {"Berserk", 3437},   {"Quazar", 2901},       {"Zurichess", 2901},  {"Deep", 2898},          {"Drofa", 2894},
     {"Daydreamer", 2890},  {"Thinker", 2885},   {"Murka", 2882},        {"The Baron", 2882},  {"Gaviota", 2871}};
 
 std::string str_tolower(std::string s) {
