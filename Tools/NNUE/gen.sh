4854c93ad1b8d0a6692d22453adaca72c1b848e2
Vivien Clauzon <xr_a_y@yahoo.fr>
2022-01-17 21:05:48 +0100

    genfen with DFRC initial position

--- Tools/NNUE/gen.sh
+++ Tools/NNUE/gen.sh
@@ -2,6 +2,6 @@
 dir=$(readlink -f $(dirname $0)/../../)
 
 for i in $(seq 1 10000); do 
-  $dir/Dist/Minic3/minic_dev_linux_x64 -selfplay 5 10000 -genFenDepth 10 -genFenDepthEG 14 -forceNNUE 1 -randomOpen 350 -FRC 1
+  $dir/Dist/Minic3/minic_dev_linux_x64 -selfplay 5 10000 -genFenDepth 10 -genFenDepthEG 14 -forceNNUE 1 -randomOpen 350 -FRC 1 -DFRC 1
 done
 
