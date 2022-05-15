28052aa56f0e1bab867c60919e88b63ed00c9750
Vivien <xr_a_y@yahoo.fr>
2022-05-15 13:48:32 +0200

    add a specific header for configuration

--- Source/definition.hpp
+++ Source/definition.hpp
@@ -40,77 +40,7 @@ typedef uint64_t u_int64_t;
 #include <unistd.h>
 #endif
 
-const std::string MinicVersion = "3.19";
-
-// *** options
-#define WITH_UCI    // include or not UCI protocol support
-#define WITH_XBOARD // include or not XBOARB protocol support
-#define WITH_MAGIC  // use magic bitboard or HB ones
-#define WITH_NNUE   // include or not NNUE support
-#define WITH_STATS  // produce or not search statitics
-#define WITH_MATERIAL_TABLE // use (and build ! cost 27Mb) or not the material table
-//#define WITH_MPI    // support "distributed" version or not
-#ifndef _MSC_VER
-#define WITH_SYZYGY // include or not syzgy ETG support
-#endif
-
-//#define WITHOUT_FILESYSTEM              // some compiler don't support whole std:filesystem
-//#define LIMIT_THREADS_TO_PHYSICAL_CORES // in order to restrict thread to the number of physical core
-//#define REPRODUCTIBLE_RESULTS           // clear state table betwwen all new search (not only all new games)
-#define WITH_NNUE_CLIPPED_RELU            // use clipped relu instead of relu for NNUE
-#ifndef __ANDROID__
-#define USE_SIMD_INTRIN                   // on simd architectures, use a hand written dot product
-#endif
-
-// *** Optim (?)
-#define USE_PARTIAL_SORT        // do not sort every move in move list
-//#define WITH_EVALSCORE_AS_INT // in fact just as slow as my basic impl ...
-
-// *** Add-ons
-#ifndef DEBUG_TOOL
-#define DEBUG_TOOL // forced
-#endif
-#define WITH_TEST_SUITE
-//#define WITH_PGN_PARSER
-
-// *** NNUE learning things
-#ifndef WITHOUT_FILESYSTEM
-#define WITH_GENFILE
-#endif
-
-#ifdef WITH_NNUE
-#ifndef __ANDROID__
-#ifndef WITHOUT_FILESYSTEM
-#define WITH_DATA2BIN
-#endif
-#endif
-#endif
-
-// *** Tuning
-//#define WITH_TIMER
-//#define WITH_SEARCH_TUNING
-//#define WITH_EVAL_TUNING
-//#define WITH_PIECE_TUNING
-
-// *** Debug
-//#define VERBOSE_EVAL
-//#define DEBUG_NNUE_UPDATE
-//#define DEBUG_BACKTRACE
-//#define DEBUG_HASH
-//#define DEBUG_PHASH
-//#define DEBUG_MATERIAL
-//#define DEBUG_APPLY
-//#define DEBUG_GENERATION
-//#define DEBUG_GENERATION_LEGAL // not compatible with DEBUG_PSEUDO_LEGAL
-//#define DEBUG_BITBOARD
-//#define DEBUG_PSEUDO_LEGAL // not compatible with DEBUG_GENERATION_LEGAL
-//#define DEBUG_HASH_ENTRY
-//#define DEBUG_KING_CAP
-//#define DEBUG_PERFT
-//#define DEBUG_STATICEVAL
-//#define DEBUG_EVALSYM
-//#define DEBUG_TT_CHECK
-//#define DEBUG_FIFTY_COLLISION
+#include "config.hpp"
 
 #ifdef WITH_EVAL_TUNING
 #define CONST_EVAL_TUNING
