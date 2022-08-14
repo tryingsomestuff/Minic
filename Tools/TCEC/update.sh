3d92812a4735a91a98cc4b381f7b2e9f6c3987ff
Vivien <xr_a_y@yahoo.fr>
2022-08-14 18:16:03 +0200

    update version

--- Tools/TCEC/update.sh
+++ Tools/TCEC/update.sh
@@ -3,7 +3,7 @@
 if [ ! -n "$VERSION" ];then
    echo "You must first set VERSION variable please, see example below"
    echo "> export VERSION=master && ./update.sh"
-   echo "> export VERSION=3.26 && ./update.sh"
+   echo "> export VERSION=3.27 && ./update.sh"
    exit 1
 fi
 
