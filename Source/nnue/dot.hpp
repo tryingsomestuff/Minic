61a019d2d8e03d56cadfa502a776c8f1ba82f31b
Vivien Clauzon <xr_a_y@yahoo.fr>
2022-08-24 15:27:03 +0200

    yet another avx512 test

--- Source/nnue/dot.hpp
+++ Source/nnue/dot.hpp
@@ -82,10 +82,15 @@ typedef __m512 v_f32_512;
 #define v_muladd_f32_512 _mm512_fmadd_ps
 
 DOT_FINLINE float v_sum_f32_512(__m512 a) {
-    __m512 tmp = _mm512_add_ps(a,_mm512_shuffle_f32x4(a,a,_MM_SHUFFLE(0,0,3,2)));
-    __m128 r = _mm512_castps512_ps128(_mm512_add_ps(tmp,_mm512_shuffle_f32x4(tmp,tmp,_MM_SHUFFLE(0,0,0,1))));
-    r = _mm_hadd_ps(r,r);
-    return _mm_cvtss_f32(_mm_hadd_ps(r,r));
+    __m512 h64 = _mm512_shuffle_f32x4(a, a, _MM_SHUFFLE(3, 2, 3, 2));
+    __m512 sum32 = _mm512_add_ps(a, h64);
+    __m512 h32 = _mm512_shuffle_f32x4(sum32, sum32, _MM_SHUFFLE(1, 0, 3, 2));
+    __m512 sum16 = _mm512_add_ps(sum32, h32);
+    __m512 h16 = _mm512_permute_ps(sum16, _MM_SHUFFLE(1, 0, 3, 2));
+    __m512 sum8 = _mm512_add_ps(sum16, h16);
+    __m512 h4 = _mm512_permute_ps(sum8, _MM_SHUFFLE(2, 3, 0, 1));
+    __m512 sum4 = _mm512_add_ps(sum8, h4);
+    return _mm_cvtss_f32(_mm512_castps512_ps128(sum4));
 }
 
 #define v_load_f32_512(PTR) _mm512_load_ps((const __m512*)(PTR))
