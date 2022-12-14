4e8ca233038c40c8e56002aaab3715737ccbc657
Vivien <xr_a_y@yahoo.fr>
2022-12-14 20:41:59 +0100

    avoid function pointer for coverage when possible

--- Source/evalTools.hpp
+++ Source/evalTools.hpp
@@ -17,10 +17,10 @@ FORCE_FINLINE void evalMob(const Position &p, BitBoard pieceBBiterator, EvalScor
 template<Color C>
 FORCE_FINLINE void evalMobQ(const Position &p, BitBoard pieceBBiterator, EvalScore &score, const BitBoard safe, const BitBoard occupancy, uint16_t & mobility) {
    BB::applyOn(pieceBBiterator, [&](const Square & k){
-      uint16_t mob = BB::countBit(BBTools::pfCoverage[P_wb - 1](k, occupancy, C) & ~p.allPieces[C] & safe);
+      uint16_t mob = BB::countBit(BBTools::coverage<P_wb>(k, occupancy, C) & ~p.allPieces[C] & safe);
       mobility += mob;
       score += EvalConfig::MOB[3][mob] * ColorSignHelper<C>();
-      mob = BB::countBit(BBTools::pfCoverage[P_wr - 1](k, occupancy, C) & ~p.allPieces[C] & safe);
+      mob = BB::countBit(BBTools::coverage<P_wr>(k, occupancy, C) & ~p.allPieces[C] & safe);
       mobility += mob;
       score += EvalConfig::MOB[4][mob] * ColorSignHelper<C>();
    });
@@ -29,7 +29,7 @@ FORCE_FINLINE void evalMobQ(const Position &p, BitBoard pieceBBiterator, EvalSco
 template<Color C>
 FORCE_FINLINE void evalMobK(const Position &p, BitBoard pieceBBiterator, EvalScore &score, const BitBoard safe, const BitBoard occupancy, uint16_t & mobility) {
    BB::applyOn(pieceBBiterator, [&](const Square & k){
-      const uint16_t mob = BB::countBit(BBTools::pfCoverage[P_wk - 1](k, occupancy, C) & ~p.allPieces[C] & safe);
+      const uint16_t mob = BB::countBit(BBTools::coverage<P_wk>(k, occupancy, C) & ~p.allPieces[C] & safe);
       mobility += mob;
       score += EvalConfig::MOB[5][mob] * ColorSignHelper<C>();
    });
