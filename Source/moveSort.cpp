2227d1c8567a50f09126f0f70c438a3b49b3749a
Vivien <xr_a_y@yahoo.fr>
2022-12-31 19:03:03 +0100

    tune move sorting

--- Source/moveSort.cpp
+++ Source/moveSort.cpp
@@ -77,13 +77,13 @@ void MoveSorter::computeScore(Move& m) const {
       else if (t == T_std || isCastling(m)) {
          if (sameMove(m, context.killerT.killers[height][0])) s += 1900; // quiet killer
          else if (sameMove(m, context.killerT.killers[height][1]))
-            s += 1700; // quiet killer
+            s += 1300; // quiet killer
          else if (height > 1 && sameMove(m, context.killerT.killers[height - 2][0]))
-            s += 1500; // quiet killer
+            s += 1700; // quiet killer
          //else if (height > 1 && sameMove(m, context.killerT.killers[height - 2][1]))
          //   s += 1400; // quiet killer
          else if (isValidMove(p.lastMove) && sameMove(context.counterT.counter[Move2From(p.lastMove)][correctedMove2ToKingDest(p.lastMove)], m)) // in sync with CounterT::update()
-            s += 1300; // quiet counter
+            s += 1500; // quiet counter
          else {
             ///@todo give another try to tune those ratio!
             // History
