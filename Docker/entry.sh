65bf13eb5779b6a484c6e5b8887422a9a9140459
Vivien Clauzon <xr_a_y@yahoo.fr>
2023-12-27 10:47:33 +0100

    update entry point

--- Docker/entry.sh
+++ Docker/entry.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
    echo "Illegal number of parameters"
    echo "\$1 shall be your username"
    echo "\$2 shall be your password"
    echo "\$3 shall be your available number of threads"
+   echo "\$4 shall be the instance name"
    exit 1
 fi
 
@@ -13,4 +14,4 @@ echo 42 > machine.txt
 echo "*************************************"
 ls
 echo "*************************************"
-python3 Client.py -U $1 -P $2 -S http://canada.x-ray.fr:8000 -T $3
+python3 client.py -U $1 -P $2 -N 1 -T $3 -I $4 -S http://canada.x-ray.fr:8000
